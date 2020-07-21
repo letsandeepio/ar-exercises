b3d6946eba352cb9b25ccee878f8981d79a11b7c
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-07-21 20:09:24 +0000

    completes exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,15 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 
-puts "Exercise 2"
-puts "----------"
+puts 'Exercise 2'
+puts '----------'
 
 # Your code goes here ...
+store1 = Store.first
+store2 = Store.find_by(id: 1)
+
+store1.name = 'Toronto'
+
+store1.save

c419dace7518fed957b229dca19947b62d31be78
Sandeep Chopra <sandeep.chopra7@gmail.com>
2020-07-21 20:05:31 +0000

    completes exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,14 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
 # Your code goes below here ...
+
+Store.create(name: 'Burnaby', annual_revenue: 300_000, womens_apparel: true, mens_apparel: true)
+Store.create(name: 'Richmond', annual_revenue: 1_260_000, womens_apparel: true, mens_apparel: false)
+Store.create(name: 'Gastown', annual_revenue: 190_000, womens_apparel: false, mens_apparel: true)
+
+puts "count #{Store.count}"
